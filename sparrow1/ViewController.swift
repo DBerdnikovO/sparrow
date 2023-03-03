e4fb14ef887ffcbd67fe2be77a566d41ad02c9fb
DBerdnikovO <95565575+DBerdnikovO@users.noreply.github.com>
2023-03-03 14:23:41 +0400

    Done

--- sparrow1/ViewController.swift
+++ sparrow1/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let gradientView = Gradient(from: .top, to: .bottom, startColor: #colorLiteral(red: 0.2955428362, green: 0.2955428362, blue: 0.2955428362, alpha: 1), endColor: .blue)
 
+    private let gradientLayer = CAGradientLayer()
+
+
+    var mainView: UIView = {
+        let view = UIView()
+        view.layer.shadowRadius = 8
+        view.layer.shadowOffset = CGSize(width: 3, height: 3)
+        view.layer.shadowOpacity = 0.5
+        view.layer.cornerRadius = 20
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+     }()
+
+     var contentsLayer: UIView = {
+        let view = Gradient(from: .top, to: .bottom, startColor: #colorLiteral(red: 0.2955428362, green: 0.2955428362, blue: 0.2955428362, alpha: 1), endColor: .blue)
+        view.layer.cornerRadius = 20
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+     }()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupView()
+
+    }
+    
+    func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(mainView)
+        mainView.addSubview(contentsLayer)
+        NSLayoutConstraint.activate([
+
+            mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            mainView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+            mainView.heightAnchor.constraint(equalToConstant: 100),
+            mainView.widthAnchor.constraint(equalToConstant: 100),
+
+            contentsLayer.centerYAnchor.constraint(equalTo: mainView.centerYAnchor),
+            contentsLayer.centerXAnchor.constraint(equalTo: mainView.centerXAnchor),
+            contentsLayer.heightAnchor.constraint(equalTo: mainView.heightAnchor),
+            contentsLayer.widthAnchor.constraint(equalTo: mainView.widthAnchor)
+        ])
     }
 
-
+    
 }
 
